b546029126c60f996a7ee6b155112ef69fcc3783
Gaurav Vishwakarma <gv211432@gmail.com>
2025-10-03 01:08:52 +0530

    updated minor configs

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('swagger', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 6000);
 }
 bootstrap();
